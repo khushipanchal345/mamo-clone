d698d781cee14bc6c313b6e26b0ce972c35b7bb8
khushipanchal345 <23bca044@gsfcuniversity.ac.in>
2024-07-17 15:17:11 +0530

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,269 +7,278 @@
 */
 
 (function() {
-  "use strict";
-
-  /**
-   * Easy selector helper function
-   */
-  const select = (el, all = false) => {
-    el = el.trim()
-    if (all) {
-      return [...document.querySelectorAll(el)]
-    } else {
-      return document.querySelector(el)
+   "use strict";
+ 
+   /**
+    * Easy selector helper function
+    */
+   const select = (el, all = false) => {
+     el = el.trim()
+     if (all) {
+       return [...document.querySelectorAll(el)]
+     } else {
+       return document.querySelector(el)
+     }
+   }
+ 
+   /**
+    * Easy event listener function
+    */
+   const on = (type, el, listener, all = false) => {
+     let selectEl = select(el, all)
+     if (selectEl) {
+       if (all) {
+         selectEl.forEach(e => e.addEventListener(type, listener))
+       } else {
+         selectEl.addEventListener(type, listener)
+       }
+     }
+   }
+ 
+   /**
+    * Mobile nav toggle
+    */
+   const toogleNav = function() {
+     let navButton = select('.nav-toggle')
+     navButton.classList.toggle('nav-toggle-active')
+     navButton.querySelector('i').classList.toggle('bx-x')
+     navButton.querySelector('i').classList.toggle('bx-menu')
+ 
+     select('.nav-menu').classList.toggle('nav-menu-active')
+   }
+   on('click', '.nav-toggle', function(e) {
+     toogleNav();
+   })
+ 
+   /**
+    * Mobile nav dropdowns activate
+    */
+   on('click', '.nav-menu .drop-down > a', function(e) {
+     e.preventDefault()
+     this.nextElementSibling.classList.toggle('drop-down-active')
+     this.parentElement.classList.toggle('active')
+   }, true)
+ 
+   /**
+    * Scrool links with a class name .scrollto
+    */
+   on('click', '.scrollto', function(e) {
+     if (select(this.hash)) {
+       select('.nav-menu .active').classList.remove('active')
+       this.parentElement.classList.toggle('active')
+       toogleNav();
+     }
+   }, true)
+ 
+ })()
+ 
+ 
+ 
+ 
+ 
+ 
+ function abc()
+ {
+ 
+    /*======Name validation*/
+ var name;
+ name=window.document.frm.name.value;
+ var thanks_msg=0;
+ 
+ name=name.toUpperCase();
+ var x=name.length;
+ var ch;
+ var count=0;
+   for(var i=0; i<=x-1; i++)
+   {
+      ch=name.charAt(i);
+      if(ch>= "A" && ch<= "z" || ch==" ")
+      {
+       count++;
+      }
+   }
+    if(count==x)
+    {
+       thanks_msg=1;
     }
-  }
-
-  /**
-   * Easy event listener function
-   */
-  const on = (type, el, listener, all = false) => {
-    let selectEl = select(el, all)
-    if (selectEl) {
-      if (all) {
-        selectEl.forEach(e => e.addEventListener(type, listener))
-      } else {
-        selectEl.addEventListener(type, listener)
-      }
+    
+    if(count!=x)
+    {
+       alert("Invalid Name");
     }
-  }
-
-  /**
-   * Mobile nav toggle
-   */
-  const toogleNav = function() {
-    let navButton = select('.nav-toggle')
-    navButton.classList.toggle('nav-toggle-active')
-    navButton.querySelector('i').classList.toggle('bx-x')
-    navButton.querySelector('i').classList.toggle('bx-menu')
-
-    select('.nav-menu').classList.toggle('nav-menu-active')
-  }
-  on('click', '.nav-toggle', function(e) {
-    toogleNav();
-  })
-
-  /**
-   * Mobile nav dropdowns activate
-   */
-  on('click', '.nav-menu .drop-down > a', function(e) {
-    e.preventDefault()
-    this.nextElementSibling.classList.toggle('drop-down-active')
-    this.parentElement.classList.toggle('active')
-  }, true)
-
-  /**
-   * Scrool links with a class name .scrollto
-   */
-  on('click', '.scrollto', function(e) {
-    if (select(this.hash)) {
-      select('.nav-menu .active').classList.remove('active')
-      this.parentElement.classList.toggle('active')
-      toogleNav();
+ 
+    if(name=="")
+    {
+       alert("Please First Enter The Name ");
     }
-  }, true)
-
-})()
-
-
-
-
-
-
-function abc()
-{
-
-   /*======Name validation*/
-var name;
-name=window.document.frm.name.value;
-var thanks_msg=0;
-
-name=name.toUpperCase();
-var x=name.length;
-var ch;
-var count=0;
-  for(var i=0; i<=x-1; i++)
-  {
-     ch=name.charAt(i);
-     if(ch>= "A" && ch<= "z" || ch==" ")
-     {
-      count++;
-     }
-  }
-   if(count==x)
+    
+    /*======Email validation*/
+    var email;
+    email=window.document.frm.email.value;
+    var n=email.length;
+    var x=email.substr(n-10,10);
+    if(x=="@gmail.com" || x=="@yahoo.com")
+    {
+       thanks_msg=5;
+    }
+    else
+    {
+       if(email=="")
+       {
+          alert("Please Enter Your E-mail");
+       }
+       else
+       {
+          alert("Invalid E-mail")
+       }
+    }
+    
+ 
+ 
+ 
+    /*=============subject validation=============*/
+ var subject;
+ subject=window.document.frm.subject.value;
+ 
+ subject=subject.toUpperCase();
+ var x=subject.length;
+ var chs;
+ var count=0;
+   for(var i=0; i<=x-1; i++)
    {
-      thanks_msg=1;
-   }
-   
-   if(count!=x)
-   {
-      alert("Invalid Name");
-   }
-
-   if(name=="")
-   {
-      alert("Please First Enter The Name ");
-   }
-   
-   /*======Email validation*/
-   var email;
-   email=window.document.frm.email.value;
-   var n=email.length;
-   var x=email.substr(n-10,10);
-   if(x=="@gmail.com" || x=="@yahoo.com")
-   {
-      thanks_msg=5;
-   }
-   else
-   {
-      if(email=="")
+      chs=subject.charAt(i);
+      if(chs>= "A" && chs<= "z" || chs==" " || chs=="_" || chs=="-")
       {
-         alert("Please Enter Your E-mail");
-      }
-      else
-      {
-         alert("Invalid E-mail")
+       count++;
       }
    }
-   
-
-
-
-   /*=============subject validation=============*/
-var subject;
-subject=window.document.frm.subject.value;
-
-subject=subject.toUpperCase();
-var x=subject.length;
-var chs;
-var count=0;
-  for(var i=0; i<=x-1; i++)
-  {
-     chs=subject.charAt(i);
-     if(chs>= "A" && chs<= "z" || chs==" " || chs=="_" || chs=="-")
-     {
-      count++;
-     }
-  }
-   if(count==x)
+    if(count==x)
+    {
+       thanks_msg=4;
+    }
+    
+    if(count!=x)
+    {
+       alert("Invalid Subject");
+    }
+ 
+    if(subject=="")
+    {
+       alert("Please Enter The Subject ");
+    }
+ 
+ /* ==============message validation======= */
+ var message;
+ message=window.document.frm.message.value;
+ 
+ message=message.toUpperCase();
+ var y=message.length;
+ var chs;
+ var count=0;
+   for(var i=0; i<=y-1; i++)
    {
-      thanks_msg=4;
-   }
-   
-   if(count!=x)
-   {
-      alert("Invalid Subject");
-   }
-
-   if(subject=="")
-   {
-      alert("Please Enter The Subject ");
-   }
-
-/* ==============message validation======= */
-var message;
-message=window.document.frm.message.value;
-
-message=message.toUpperCase();
-var y=message.length;
-var chs;
-var count=0;
-  for(var i=0; i<=y-1; i++)
-  {
-     chs=message.charAt(i);
-     if(chs>= "A" && chs<= "z" || chs==" " || chs=="_" || chs=="-")
-     {
-      count++;
-     }
-  }
-   if(count==y)
-   {
-      thanks_msg=3;
-   }
-   
-   if(count!=y)
-   {
-      alert("Invalid Message");
-   }
-
-   if(message=="")
-   {
-      alert("Please Enter The Message ");
-   }
-   
-   if(count++)
-   {
-      alert("Sent Sucessfully");
-      alert("THANK YOU "+name);     
-   }
-}
-
-
-
-
-
-
-
-function xyz()
-{
-
-   /*======Name validation*/
-var name;
-name=window.document.frm1.name.value;
-var thanks_msg=0;
-
-name=name.toUpperCase();
-var x=name.length;
-var ch;
-var count=0;
-  for(var i=0; i<=x-1; i++)
-  {
-     ch=name.charAt(i);
-     if(ch>= "A" && ch<= "z" || ch==" ")
-     {
-      count++;
-     }
-  }
-   if(count==x)
-   {
-      thanks_msg=1;
-   }
-   
-   if(count!=x)
-   {
-      alert("Invalid Name");
-   }
-
-   if(name=="")
-   {
-      alert("Please First Enter The Name!");
-   }
-   
-   /*======Email validation*/
-   var email;
-   email=window.document.frm1.email.value;
-   var n=email.length;
-   var x=email.substr(n-10,10);
-   if(x=="@gmail.com" || x=="@yahoo.com")
-   {
-      thanks_msg=5;
-   }
-   else
-   {
-      if(email=="")
+      chs=message.charAt(i);
+      if(chs>= "A" && chs<= "z" || chs==" " || chs=="_" || chs=="-")
       {
-         alert("Enter Your E-mail");
-      }
-      else
-      {
-         alert("Invalid E-mail")
+       count++;
       }
    }
-   
-   if(count++)
+    if(count==y)
+    {
+       thanks_msg=3;
+    }
+    
+    if(count!=y)
+    {
+       alert("Invalid Message");
+    }
+ 
+    if(message=="")
+    {
+       alert("Please Enter The Message ");
+    }
+    
+    if(count++)
+    {
+       alert("Sent Sucessfully");
+       alert("THANK YOU "+name);     
+    }
+ }
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ function xyz()
+ {
+ 
+    /*======Name validation*/
+ var name;
+ name=window.document.frm1.name.value;
+ var thanks_msg=0;
+ 
+ name=name.toUpperCase();
+ var x=name.length;
+ var ch;
+ var count=0;
+   for(var i=0; i<=x-1; i++)
    {
-      alert("Sent Sucessfully");
-      alert("THANK YOU "+name);     
+      ch=name.charAt(i);
+      if(ch>= "A" && ch<= "z" || ch==" ")
+      {
+       count++;
+      }
    }
-}
+    if(count==x)
+    {
+       thanks_msg=1;
+    }
+    
+    if(count!=x)
+    {
+       alert("Invalid Name");
+    }
+ 
+    if(name=="")
+    {
+       alert("Please First Enter The Name!");
+    }
+    
+    /*======Email validation*/
+    var email;
+    email=window.document.frm1.email.value;
+    var n=email.length;
+    var x=email.substr(n-10,10);
+    if(x=="@gmail.com" || x=="@yahoo.com")
+    {
+       thanks_msg=5;
+    }
+    else
+    {
+       if(email=="")
+       {
+          alert("Enter Your E-mail");
+       }
+       else
+       {
+          alert("Invalid E-mail")
+       }
+    }
+    
+    if(count++)
+    {
+       alert("Sent Sucessfully");
+       alert("THANK YOU "+name);     
+    }
+ }
+ 
+ 
+ 
